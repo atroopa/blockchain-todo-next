00dba8bb85e810d6dafde7eb01eeb957a07779c5
atroopa <atroopa@gmail.com>
2023-05-18 17:59:27 +0330

    form ready to use

--- pages/index.js
+++ pages/index.js
@@ -24,23 +24,38 @@ export default function Home() {
 
 
   return (
-    <div className="flex felx-col justify-center">
+    <div className="flex flex-col  items-center justify-center">
       <div className=" text-5xl font-bold py-10">
         Welcome to decenteralize To-do App 
       </div>
-
         <form className="w-full max-w-sm">
           <div className="flex items-center border-b border-teal-500 py-2">
-            <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Jane Doe" aria-label="Full name" />
-            <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
-              Sign Up
+            <input className="appearance-none 
+                              bg-transparent 
+                              border-none 
+                              w-full 
+                              text-gray-700 
+                              mr-3 py-1 px-2 
+                              leading-tight 
+                              focus:outline-none" 
+                              type="text" 
+                              aria-label="Full name" />
+            <button className="flex-shrink-0 
+                             bg-teal-500 
+                             hover:bg-teal-700 
+                             border-teal-500 
+                             hover:border-teal-700 
+                             text-sm border-4 
+                             text-white 
+                             py-1 px-2 rounded" 
+                             type="button">
+                             add
             </button>
             <button className="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded" type="button">
-              Cancel
+              remove
             </button>
           </div>
         </form>
-
 
     </div>
   )
