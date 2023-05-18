69831aa836553de7c3e15a1251b1ef7b88f1d9c8
atroopa <atroopa@gmail.com>
2023-05-18 17:34:18 +0330

    app ready to work

---
+++ Utils/config.js
@@ -0,0 +1,110 @@
+export const API_URL = "http://127.0.0.1:7545";
+// 0xDA967F93dE50366f0c36B1a3fa8F5CdFF0Eb43f1
+export const PRIVATE_KEY = "0x89b25a90086d8158597442ad3ff9ec579066880d2c91296298fb835c58b20684";
+export const contractAddress = "0x47d0e8376849DF890cEb524c5b15A153697dA479";
+export const contractAbi = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_desc",
+				"type": "string"
+			}
+		],
+		"name": "addTask",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			}
+		],
+		"name": "markAsFinished",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "getAllTasks",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "desc",
+						"type": "string"
+					},
+					{
+						"internalType": "enum TaskToDo.TaskStatus",
+						"name": "status",
+						"type": "uint8"
+					}
+				],
+				"internalType": "struct TaskToDo.Task[]",
+				"name": "",
+				"type": "tuple[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			}
+		],
+		"name": "getTask",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "enum TaskToDo.TaskStatus",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "tasks",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "desc",
+				"type": "string"
+			},
+			{
+				"internalType": "enum TaskToDo.TaskStatus",
+				"name": "status",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
