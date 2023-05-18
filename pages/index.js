ec06e7259b6f5151b21fd615bbbedf00fd7943c6
atroopa <atroopa@gmail.com>
2023-05-18 17:52:51 +0330

    justify center

--- pages/index.js
+++ pages/index.js
@@ -24,22 +24,22 @@ export default function Home() {
 
 
   return (
-    <div>
-      <div className="flex felx-col justify-center text-5xl font-bold py-10">
+    <div className="flex felx-col justify-center">
+      <div className=" text-5xl font-bold py-10">
         Welcome to decenteralize To-do App 
       </div>
 
-<form className="w-full max-w-sm">
-  <div className="flex items-center border-b border-teal-500 py-2">
-    <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Jane Doe" aria-label="Full name" />
-    <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
-      Sign Up
-    </button>
-    <button className="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded" type="button">
-      Cancel
-    </button>
-  </div>
-</form>
+        <form className="w-full max-w-sm">
+          <div className="flex items-center border-b border-teal-500 py-2">
+            <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Jane Doe" aria-label="Full name" />
+            <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+              Sign Up
+            </button>
+            <button className="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded" type="button">
+              Cancel
+            </button>
+          </div>
+        </form>
 
 
     </div>
